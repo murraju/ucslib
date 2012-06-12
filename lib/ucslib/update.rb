2989e72065b0dc2df004538ba918f19f85529fef
Murali Raju <murali.raju@appliv.com>
2012-06-12 09:48:41 -0400

    update class first take

--- lib/ucslib/update.rb
+++ lib/ucslib/update.rb
@@ -25,4 +25,43 @@ class UCSUpdate
 
   end
   
+  def update_host_firmware_package(json)
+  
+		host_firmware_pkg_name  = JSON.parse(json)['host_firmware_pkg_name']
+		hardware_model          = JSON.parse(json)['hardware_model'].to_s
+		hardware_type           = JSON.parse(json)['hardware_type']
+		hardware_vendor         = JSON.parse(json)['hardware_vendor'].to_s
+		firmware_version        = JSON.parse(json)['firmware_version'].to_s
+		org                     = JSON.parse(json)['org']
+
+    xml_builder = Nokogiri::XML::Builder.new do |xml|
+      xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'false'){
+        xml.inConfigs{
+          xml.pair('key' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}"){
+            xml.firmwareComputeHostPack('descr' => '', 'dn' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}",
+                                        'ignoreCompCheck' => 'yes', 'mode' => 'staged', 'stageSize' => '0', 'updateTrigger' => 'immediate'){
+                                         xml.firmwarePackItem('hwModel' => "#{hardware_model}", 'hwVendor' => "#{hardware_vendor}",
+                                                              'rn' => "pack-image-#{hardware_vendor}|#{hardware_model}|#{hardware_type}",
+                                                              'type' => "#{hardware_type}", 'version' => "#{firmware_version}")
+                                       }
+          }
+        }
+      }
+    end
+
+    #Create XML
+
+    update_host_firmware_packageXML = xml_builder.to_xml.to_s
+
+    #Post
+
+    begin
+      RestClient.post(@url, update_host_firmware_packageXML, :content_type => 'text/xml').body
+    rescue Exception => e
+      raise "Error #{e}"
+    end
+
+  end
+
+  
 end
