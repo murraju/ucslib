b594fde60f243b95e7aacb4e0db926bf68f48044
Murali Raju <murali.raju@appliv.com>
2012-11-07 12:17:39 -0500

    Fixed Hostfirmware packaga creation with an availability of a toggle flag

--- lib/ucslib/provision.rb
+++ lib/ucslib/provision.rb
@@ -464,39 +464,44 @@ class UCSProvision
   		hardware_vendor               = JSON.parse(json)['hardware_vendor'].to_s
   		firmware_version              = JSON.parse(json)['firmware_version'].to_s
   		org                           = JSON.parse(json)['org']
+      flag                          = JSON.parse(json)['flag']
+
+      unless flag == 'update'
 	  
-      xml_builder = Nokogiri::XML::Builder.new do |xml|
-        xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'true'){
-          xml.inConfigs{
-            xml.pair('key' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}"){
-              xml.firmwareComputeHostPack('descr' => "#{host_firmware_pkg_description}", 'dn' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}",
-                                         'ignoreCompCheck' => 'yes', 'mode' => 'staged', 'name' => "#{host_firmware_pkg_name}", 'stageSize' => '0',
-                                         'status' => 'created', 'updateTrigger' => 'immediate'){
-                                           xml.firmwarePackItem('hwModel' => "#{hardware_model}", 'hwVendor' => "#{hardware_vendor}",
-                                                                'rn' => "pack-image-#{hardware_vendor}|#{hardware_model}|#{hardware_type}",
-                                                                'type' => "#{hardware_type}", 'version' => "#{firmware_version}")
-                                         }
+        xml_builder = Nokogiri::XML::Builder.new do |xml|
+          xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'true'){
+            xml.inConfigs{
+              xml.pair('key' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}"){
+                xml.firmwareComputeHostPack('descr' => "#{host_firmware_pkg_description}", 'dn' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}",
+                                           'ignoreCompCheck' => 'yes', 'mode' => 'staged', 'name' => "#{host_firmware_pkg_name}", 'stageSize' => '0',
+                                           'status' => 'created', 'updateTrigger' => 'immediate'){
+                                             xml.firmwarePackItem('hwModel' => "#{hardware_model}", 'hwVendor' => "#{hardware_vendor}",
+                                                                  'rn' => "pack-image-#{hardware_vendor}|#{hardware_model}|#{hardware_type}",
+                                                                  'type' => "#{hardware_type}", 'version' => "#{firmware_version}")
+                                           }
+              }
             }
           }
-        }
+        end
+
+      else
+              
+        xml_builder = Nokogiri::XML::Builder.new do |xml|
+          xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'false'){
+            xml.inConfigs{
+              xml.pair('key' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}"){
+                xml.firmwareComputeHostPack('descr' => "#{host_firmware_pkg_description}", 'dn' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}",
+                                            'ignoreCompCheck' => 'yes', 'mode' => 'staged', 'stageSize' => '0', 'updateTrigger' => 'immediate'){
+                                             xml.firmwarePackItem('hwModel' => "#{hardware_model}", 'hwVendor' => "#{hardware_vendor}",
+                                                                  'rn' => "pack-image-#{hardware_vendor}|#{hardware_model}|#{hardware_type}",
+                                                                  'type' => "#{hardware_type}", 'version' => "#{firmware_version}")
+                                           }
+              }
+            }
+          }
+        end
+
       end
-
-      
-      # xml_builder = Nokogiri::XML::Builder.new do |xml|
-      #   xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'false'){
-      #     xml.inConfigs{
-      #       xml.pair('key' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}"){
-      #         xml.firmwareComputeHostPack('descr' => '', 'dn' => "org-root/org-#{org}/fw-host-pack-#{host_firmware_pkg_name}",
-      #                                     'ignoreCompCheck' => 'yes', 'mode' => 'staged', 'stageSize' => '0', 'updateTrigger' => 'immediate'){
-      #                                      xml.firmwarePackItem('hwModel' => "#{hardware_model}", 'hwVendor' => "#{hardware_vendor}",
-      #                                                           'rn' => "pack-image-#{hardware_vendor}|#{hardware_model}|#{hardware_type}",
-      #                                                           'type' => "#{hardware_type}", 'version' => "#{firmware_version}")
-      #                                    }
-      #       }
-      #     }
-      #   }
-      # end
-
 
 
       #Create XML
