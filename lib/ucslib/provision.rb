82acd680cb7a9245c9316b4c955b7f7a87ac862a
Murali Raju <murali.raju@appliv.com>
2012-11-09 11:45:45 -0500

    set_vhba_template recfactor to include description

--- lib/ucslib/provision.rb
+++ lib/ucslib/provision.rb
@@ -810,12 +810,13 @@ class UCSProvision
   		switch             = JSON.parse(json)['switch']
   		vsan_name          = JSON.parse(json)['vsan_name']
   		org  			         = JSON.parse(json)['org']
+      description        = JSON.parse(json)['description']
 
   		xml_builder = Nokogiri::XML::Builder.new do |xml|
   		  xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'true'){
   		    xml.inConfigs{
   		      xml.pair('key' => "org-root/org-#{org}/san-conn-templ-#{vbha_template_name}"){
-  		        xml.vnicSanConnTempl('descr' => '', 'dn' => "org-root/org-#{org}/san-conn-templ-#{vbha_template_name}",
+  		        xml.vnicSanConnTempl('descr' => "#{description}", 'dn' => "org-root/org-#{org}/san-conn-templ-#{vbha_template_name}",
   		                             'identPoolName' => "#{wwpn_pool}", 'maxDataFieldSize' => '2048', 'name' => "#{vbha_template_name}",
   		                             'pinToGroupName' => '', 'qosPolicyName' => '', 'statsPolicyName' => 'default', 'status' => 'created',
   		                             'switchId' => "#{switch}", 'templType' => 'updating-template'){
