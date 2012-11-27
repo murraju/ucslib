6e56e0787d1b21d12c4815f45d4ed37392f3ac6f
Murali Raju <murali.raju@appliv.com>
2012-11-27 13:20:44 -0500

    Scaffold for getting state information via discover_state in Manage.rb

--- lib/ucslib/manage.rb
+++ lib/ucslib/manage.rb
@@ -25,45 +25,73 @@ class UCSManage
 
 	end
 
+
+	def discover_state
+    
+	    #Start Build of the Multi-Class XML for interogating state
+	    xml_builder = Nokogiri::XML::Builder.new do |xml|
+	       xml.configResolveClasses('cookie' => @cookie, 'inHierarchical' => 'false') {
+	        xml.inIds{
+	          xml.classId("value" => "topSystem")
+	          xml.classId("value" => "macpoolPool")
+	        } 
+	       }
+	    end
+
+	    #End Build Multi-Class XML
+
+	    ucs_multi_class_state_xml = xml_builder.to_xml.to_s
+	    ucs_response_multi_class_state = RestClient.post(@url, ucs_multi_class_state_xml, :content_type => 'text/xml').body
+
+		#Uncomment the following to create a dump to review and debug elements
+		fh = File.new("ucs_response_multiclass_state.xml", "w")
+		fh.puts ucs_response_multi_class_state.inspect 
+		fh.close
+	     
+	    Nokogiri::XML(ucs_response_multi_class_state)		
+		
+	end
+		
+
 	def associate_service_profile_template_to_server_pool(json)
 
-	service_profile_boot_policy        = JSON.parse(json)['service_profile_boot_policy']
-	service_profile_host_fw_policy     = JSON.parse(json)['service_profile_host_fw_policy']
-	service_profile_mgmt_fw_policy     = JSON.parse(json)['service_profile_mgmt_fw_policy']
-	service_profile_uuid_pool          = JSON.parse(json)['service_profile_uuid_pool']
-	service_profile_template_to_bind   = JSON.parse(json)['service_profile_template_to_bind']
-	service_profile_server_pool 	   = JSON.parse(json)['service_profile_server_pool']
-	org                				   = JSON.parse(json)['org']
+		service_profile_boot_policy        = JSON.parse(json)['service_profile_boot_policy']
+		service_profile_host_fw_policy     = JSON.parse(json)['service_profile_host_fw_policy']
+		service_profile_mgmt_fw_policy     = JSON.parse(json)['service_profile_mgmt_fw_policy']
+		service_profile_uuid_pool          = JSON.parse(json)['service_profile_uuid_pool']
+		service_profile_template_to_bind   = JSON.parse(json)['service_profile_template_to_bind']
+		service_profile_server_pool 	   = JSON.parse(json)['service_profile_server_pool']
+		org                				   = JSON.parse(json)['org']
 
 
-	xml_builder = Nokogiri::XML::Builder.new do |xml|
-	  xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'true'){
-	    xml.inConfigs{
-	      xml.pair('key' => "org-root/org-#{org}/ls-#{service_profile_template_to_bind}"){
-	        xml.lsServer('agentPolicyName' => '', 'biosProfileName' => '', 'bootPolicyName' => "#{service_profile_boot_policy}",
-	                     'descr' => '', 'dn' => "org-root/org-#{org}/ls-#{service_profile_template_to_bind}",
-	                     'dynamicConPolicyName' => '', 'extIPState' => 'none', 'hostFwPolicyName' => "#{service_profile_host_fw_policy}",
-	                     'identPoolName' => "#{service_profile_uuid_pool}", 'localDiskPolicyName' => 'default', 'maintPolicyName' => 'default',
-	                     'mgmtAccessPolicyName' => '', 'mgmtFwPolicyName' => "#{service_profile_mgmt_fw_policy}", 'powerPolicyName' => 'default', 
-	                     'scrubPolicyName' => '', 'solPolicyName' => 'default', 'srcTemplName' => '', 
-	                     'statsPolicyName' => 'default', 'status' => 'created,modified', 'usrLbl' => '', 'uuid' => '0', 'vconProfileName' => ''){
-	                       xml.lsRequirement('name' => "#{service_profile_server_pool}", 'qualifier' => '', 'restrictMigration' => 'no', 'rn' => 'pn-req')
-	                     }
-	      }
-	    }
-	  }
-	end
+		xml_builder = Nokogiri::XML::Builder.new do |xml|
+		  xml.configConfMos('cookie' => "#{@cookie}", 'inHierarchical' => 'true'){
+		    xml.inConfigs{
+		      xml.pair('key' => "org-root/org-#{org}/ls-#{service_profile_template_to_bind}"){
+		        xml.lsServer('agentPolicyName' => '', 'biosProfileName' => '', 'bootPolicyName' => "#{service_profile_boot_policy}",
+		                     'descr' => '', 'dn' => "org-root/org-#{org}/ls-#{service_profile_template_to_bind}",
+		                     'dynamicConPolicyName' => '', 'extIPState' => 'none', 'hostFwPolicyName' => "#{service_profile_host_fw_policy}",
+		                     'identPoolName' => "#{service_profile_uuid_pool}", 'localDiskPolicyName' => 'default', 'maintPolicyName' => 'default',
+		                     'mgmtAccessPolicyName' => '', 'mgmtFwPolicyName' => "#{service_profile_mgmt_fw_policy}", 'powerPolicyName' => 'default', 
+		                     'scrubPolicyName' => '', 'solPolicyName' => 'default', 'srcTemplName' => '', 
+		                     'statsPolicyName' => 'default', 'status' => 'created,modified', 'usrLbl' => '', 'uuid' => '0', 'vconProfileName' => ''){
+		                       xml.lsRequirement('name' => "#{service_profile_server_pool}", 'qualifier' => '', 'restrictMigration' => 'no', 'rn' => 'pn-req')
+		                     }
+		      }
+		    }
+		  }
+		end
 
-	#Create XML
+		#Create XML
 
-	associate_service_profile_template_to_server_pool_xml = xml_builder.to_xml.to_s
+		associate_service_profile_template_to_server_pool_xml = xml_builder.to_xml.to_s
 
-	#Post 
-	begin
-		RestClient.post(@url, associate_service_profile_template_to_server_pool_xml, :content_type => 'text/xml').body
-	rescue Exception => e
-		raise "Error #{e}"
-	end 
+		#Post 
+		begin
+			RestClient.post(@url, associate_service_profile_template_to_server_pool_xml, :content_type => 'text/xml').body
+		rescue Exception => e
+			raise "Error #{e}"
+		end 
 
 	end
 end
